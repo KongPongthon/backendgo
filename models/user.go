8cf07cb5aee9a3f289873a2250a6cfb6a2de4689
TLO-01 <>
2025-10-17 16:55:46 +0700

    schema User

--- models/user.go
+++ models/user.go
@@ -33,4 +33,33 @@ type UserDetail struct {
     Position    string
     Department  string
     Status      UserStatus `gorm:"type:varchar(10)"`
+    Salary       float32
+    TimeWork []TimeWork
+    Leave []Leave
+}
+
+type TimeWork struct {
+    ID uuid.UUID `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
+    UserDetail uint
+    TimeIn float32
+    TimeOut float32
+    TotalWorkTime string
+    Date string `gorm:"type:DATE"`
+}
+
+type LeaveStatus string
+
+const (
+    StatusPending LeaveStatus = "pending"
+    StatusApproved LeaveStatus = "approved"
+    StatusRejected LeaveStatus = "rejected"
+)
+type Leave struct {
+    ID uuid.UUID `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
+    UserDetail uint
+    StartDate string `gorm:"type:DATE"`
+    EndDate string `gorm:"type:DATE"`
+    Description string
+    Total float32
+    Status LeaveStatus `gorm:"type:varchar(10)"`
 }
